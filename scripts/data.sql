d368d217cef54557fe0f46e1aa5ba507f1ff486f
Kiet Dang <dakiet@gmail.com>
2020-11-20 08:16:32 -0800

    update data.sql

---
+++ scripts/data.sql
@@ -0,0 +1,4 @@
+UPDATE `Room` SET `Room_Status_ID` = '3' WHERE `Room`.`Room_Number` = 301; 
+UPDATE `Room` SET `Room_Status_ID` = '3' WHERE `Room`.`Room_Number` = 404; 
+UPDATE `Room` SET `Room_Status_ID` = '2' WHERE `Room`.`Room_Number` = 204; 
+UPDATE `Room` SET `Room_Status_ID` = '2' WHERE `Room`.`Room_Number` = 401; 
