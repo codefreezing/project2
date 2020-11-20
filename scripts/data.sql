840e649b2c856b1995f708dc936a8e6ce16264dd
Kiet Dang <dakiet@gmail.com>
2020-11-20 15:02:43 -0800

    update.sql

--- scripts/data.sql
+++ scripts/data.sql
@@ -8,4 +8,4 @@ UPDATE `Invoice` SET `Date_Checkout` = '2020-11-14' WHERE `Invoice`.`Invoice_ID`
 UPDATE `Invoice` SET `Date_Checkout` = '2020-11-11' WHERE `Invoice`.`Invoice_ID` = 4; 
 UPDATE `Invoice` SET `Date_Checkout` = '2020-12-26' WHERE `Invoice`.`Invoice_ID` = 5; 
 UPDATE `Invoice` SET `Date_Checkout` = '2020-12-24' WHERE `Invoice`.`Invoice_ID` = 6; 
-UPDATE `Invoice` SET `Date_Checkout` = '2020-12-25' WHERE `Invoice`.`Invoice_ID` = 7; đúng
+UPDATE `Invoice` SET `Date_Checkout` = '2020-12-25' WHERE `Invoice`.`Invoice_ID` = 7;
